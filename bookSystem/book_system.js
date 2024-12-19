738d70e6d1ac274c868d5f9c86fe87b04c11a240
Evan <evan.ouell@gmail.com>
2024-12-19 14:46:47 -0500

    Delete

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -26,7 +26,8 @@ function showbooks() {
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="deleteBook(${index})">Delete Book</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -36,4 +37,9 @@ function showbooks() {
     document.getElementById('authorName').value = '';
     document.getElementById('bookDescription').value = '';
     document.getElementById('pagesNumber').value = '';
+ }
+
+ function deleteBook(index){
+    books.splice(index, 1);
+    showbooks();
  }
