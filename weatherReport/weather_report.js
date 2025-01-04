13c33bbb30e4862b60627fa3781dcccea5414726
Evan <evan.ouell@gmail.com>
2025-01-03 19:30:23 -0500

    lat and lon

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -21,4 +21,29 @@ function showweatherDetails(event) {
     
 }
 
+function showweatherDetailsCoords(event) {
+    event.preventDefault();
+
+    const lat = document.getElementById('latitude').value;
+    const lon = document.getElementById('longitude').value;
+    const apiKey = '05cfbdfb0c66fea5e6978c472ef4ffa8'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const weatherInfo = document.getElementById('weatherInfo');
+      weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                              <p>Temperature: ${data.main.temp} &#8451;</p>
+                              <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch(error => {
+        console.error('Error fetching weather:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+      });
+    
+}
+
 document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+document.getElementById('weatherFormCoords').addEventListener('submit',showweatherDetailsCoords );
